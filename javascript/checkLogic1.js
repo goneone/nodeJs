a290a934f54b1d90f394d0fdda811ba9cab0a9e1
kuyt1819 <kuyt1819@gmail.com>
2018-07-12 16:47:53 +0900

    test

---
+++ javascript/checkLogic1.js
@@ -0,0 +1,13 @@
+package example;
+
+public class checkLogic1 {
+//년도가 2018년,2019년,2020,2021,2022등 있고
+//컬럼에 일자, 특별일db(특별일1,특별일2,특별일3 선택하는 탭), 특별일명(텍스트)이 있음.
+//날짜가 안겹치고, 만약 날짜가 겹치면 특별일이 안겹치는 로직.
+
+	public static void main(String[] args) {
+		for (var i = 0; i < 10; i++) {
+			
+		}
+	}
+}
