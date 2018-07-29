43cf4094c7fc71d52985f4da36fbf29faf131acf
kuyt1819 <kuyt1819@gmail.com>
2018-07-29 23:39:29 +0900

    dd

--- nodejs/node.js-mysql-1/lib/author.js
+++ nodejs/node.js-mysql-1/lib/author.js
@@ -1,15 +1,46 @@
-
+var db = require('./db');
+var template = require('./template.js');
 
 exports.home = function(request, response){
     db.query(`SELECT * FROM topic`, function(error,topics){ //여기서 function은 쿼리문이 실행된후에 실행될 콜백 함수임
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = template.list(topics); // template.js에 있는 list프로퍼티의 함수
-        var html = template.HTML(title, list, //template.html은 웹페이지의 가장 큰틀의 html코드를 만들어줌
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
-    });
+        db.query(`SELECT * FROM author`, function(error2,authors){
+
+            var tag = '';
+
+            var title = 'author';
+            var list = template.list(topics); // template.js에 있는 list프로퍼티의 함수
+            var html = template.HTML(title, list, //template.html은 웹페이지의 가장 큰틀의 html코드를 만들어줌
+              `
+              <table>
+                <tr>
+                  <td>test</td>
+                  <td>test1</td>
+                  <td>test2</td>
+                </tr>
+                <tr>
+                  <td>test</td>
+                  <td>test1</td>
+                  <td>test2</td>
+                </tr>
+                <tr>
+                  <td>test</td>
+                  <td>test1</td>
+                  <td>test2</td>
+                </tr>
+              </table>
+              <style>
+                table {
+                  border-collapse:collapse;
+                }
+                td{
+                  border: 1px solid black;
+                }
+              </style>
+              `,
+              `<a href="/create">create</a>`
+            );
+            response.writeHead(200);
+            response.end(html);
+        });
+      });
 }
