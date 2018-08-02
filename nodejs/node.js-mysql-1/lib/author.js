822b1f2e624967687dc257bc1661810db5daddc7
kuyt1819 <kuyt1819@gmail.com>
2018-08-03 00:43:49 +0900

    nodejs --mysql author update2

--- nodejs/node.js-mysql-1/lib/author.js
+++ nodejs/node.js-mysql-1/lib/author.js
@@ -27,7 +27,7 @@ exports.home = function(request, response){
                 <textarea name="profile" placeholder="description"></textarea>
                </p>
                <p>
-                <input type="submit">
+                <input type="submit" value="create">
                </p>
               </form>
               `,
@@ -72,9 +72,9 @@ exports.create_process = function(request, response){
 exports.update = function(request, response){
     db.query(`SELECT * FROM topic`, function(error,topics){ //여기서 function은 쿼리문이 실행된후에 실행될 콜백 함수임
         db.query(`SELECT * FROM author`, function(error2,authors){
-            console.log(authors)
-            var _url = request.url;
-            var queryData = url.parse(_url, true).query; //url을 분석하는 코드.
+          var _url = request.url;
+          var queryData = url.parse(_url, true).query; //url을 분석하는 코드.
+          db.query(`SELECT * FROM author WHERE id=?`, [queryData.id], function(error3,author){
             var title = 'author';
             var list = template.list(topics); // template.js에 있는 list프로퍼티의 함수
             var html = template.HTML(title, list, //template.html은 웹페이지의 가장 큰틀의 html코드를 만들어줌
@@ -94,13 +94,13 @@ exports.update = function(request, response){
                 <input type="hidden" name="id" value="${queryData.id}">
                </p>
                <p>
-                <input type="text" name="name" placeholder="name">
+                <input type="text" name="name" value="${author[0].name}" placeholder="name">
                </p>
                <p>
-                <textarea name="profile" placeholder="description"></textarea>
+                <textarea name="profile" placeholder="description">${author[0].profile}</textarea>
                </p>
                <p>
-                <input type="submit">
+                <input type="submit" value="update">
                </p>
               </form>
               `,
@@ -108,6 +108,36 @@ exports.update = function(request, response){
             );
             response.writeHead(200);
             response.end(html);
+          });
         });
+      });
+}
+
+exports.update_process = function(request, response){
+    var body = '';
+      request.on('data', function(data){
+          body = body + data;
+          console.log("-----------/author/update_process log------------------");
+          console.log(body); //화면에서 입력한 값.
+          console.log("-----------/author/update_process log------------------");
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          console.log("-----------222/author/update_process log------------------");
+          console.log(post);
+          console.log("-----------222/author/update_process log------------------");
+          db.query(`
+            UPDATE author SET name=?, profile=? WHERE ID=?`,
+            [post.name, post.profile, post.id],
+            function(error, result){
+              if(error){
+                throw error;
+              }
+              response.writeHead(302, {Location: `/author`});
+              //200은 성공, 302는 다른페이지로 리다이렉션시키라는 뜻
+              //create를 한 다음에 그 생성한 페이지로 이동하게끔!
+              response.end();
+            }
+          )
       });
 }
