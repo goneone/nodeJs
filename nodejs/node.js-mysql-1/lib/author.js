539514fb735fcdd5f7f40fe543a78de3b53d135d
kuyt1819 <kuyt1819@gmail.com>
2018-08-20 09:25:04 +0900

    nodejs 게시판 검색창 추가-2 왜 get방식으로 해야하나..?post방식은 안되나?

--- nodejs/node.js-mysql-1/lib/author.js
+++ nodejs/node.js-mysql-1/lib/author.js
@@ -20,12 +20,12 @@ exports.home = function(request, response){
                   border: 1px solid black;
                 }
               </style>
-              <form action="/author/search_author_process" method="get">
+              <form action="/author/search_author_process" method="post">
                <p>
-                <input type="text" name="search" placeholder="Search!">
+                <input type="text" name="searchAuthorName" placeholder="Search Name!">
                </p>
                <p>
-                <input type="submit" value="Search">
+                <input type="submit" value="searchAuthorName">
                </p>
               </form>
 
@@ -212,23 +212,69 @@ exports.customizing_delete_process = function(request, response){
       }
 
 //내가만든 검색창..
+/*
+exports.home = function(request, response){
+    db.query(`SELECT * FROM topic`, function(error,topics){ //여기서 function은 쿼리문이 실행된후에 실행될 콜백 함수임
+        db.query(`SELECT * FROM author`, function(error2,authors){
+            var title = 'author';
+            var list = template.list(topics); // template.js에 있는 list프로퍼티의 함수
+            var html = template.HTML(title, list, //template.html은 웹페이지의 가장 큰틀의 html코드를 만들어줌
+              `
+                ${template.authorTable(authors)}
+              <style>
+                table {
+                  border-collapse:collapse;
+                }
+                td{
+                  border: 1px solid black;
+                }
+              </style>
+              <form action="/author/search_author_process" method="get">
+               <p>
+                <input type="text" name="search" placeholder="Search!">
+               </p>
+               <p>
+                <input type="submit" value="Search">
+               </p>
+              </form>
+
+              <form action="/author/create_process" method="post">
+               <p>
+                <input type="text" name="name" placeholder="name">
+               </p>
+               <p>
+                <textarea name="profile" placeholder="description"></textarea>
+               </p>
+               <p>
+                <input type="submit" value="create">
+               </p>
+              </form>
+              `,
+              ``
+            );
+            response.writeHead(200);
+            response.end(html);
+        });
+      });
+}
+*/
+
 exports.search_author_process = function(request, response){
     var body = '';
       request.on('data', function(data){
           body = body + data;
-          console.log("-----------/author/create_process log------------------");
+          console.log("-----------/author/search_author_process log------------------");
           console.log(body); //화면에서 입력한 값.
-          console.log("-----------/author/create_process log------------------");
+          console.log("-----------/author/search_author_process log------------------");
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          console.log("-----------222/author/create_process log------------------");
+          console.log("-----------222/author/search_author_process log------------------");
           console.log(post);
-          console.log("-----------222/author/create_process log------------------");
+          console.log("-----------222/author/search_author_process log------------------");
           db.query(`
-            INSERT INTO author (name, profile)
-              VALUES(?, ?)`,
-            [post.name, post.profile],
+            SELECT * FROM author where name=?`,
+            [post.searchAuthorName],
             function(error, result){
               if(error){
                 throw error;
@@ -237,6 +283,7 @@ exports.search_author_process = function(request, response){
               //200은 성공, 302는 다른페이지로 리다이렉션시키라는 뜻
               //create를 한 다음에 그 생성한 페이지로 이동하게끔!
               response.end();
+              //SELECT * FROM TOPIC WHERE TITLE=”KEYWORD”
             }
           )
       });
