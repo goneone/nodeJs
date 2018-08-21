145c4354d27b20900d316114e7f2a19ddbfc9772
kuyt1819 <kuyt1819@gmail.com>
2018-08-21 15:01:17 +0900

    nodejs 검색창 완성~~~~~~~

--- nodejs/node.js-mysql-1/lib/author.js
+++ nodejs/node.js-mysql-1/lib/author.js
@@ -214,27 +214,18 @@ exports.customizing_delete_process = function(request, response){
       }
 
 //내가만든 검색창..
-
 exports.search_author = function(request, response){
     var body = '';
     request.on('data', function(data){
         body = body + data;
-        console.log("-----------create_process log------------------");
-        console.log(body); //화면에서 입력한 값.
-        console.log("-----------create_process log------------------");
     });
     request.on('end', function(){
         var post = qs.parse(body);
-        console.log("-----------222create_process log------------------");
+        console.log("-----------search_author log------------------");
         console.log(post);
-        console.log("-----------2222create_process log------------------");
+        console.log("-----------search_author log------------------");
     db.query(`SELECT * FROM topic`, function(error,topics){ //여기서 function은 쿼리문이 실행된후에 실행될 콜백 함수임
-        //db.query(`SELECT * FROM author where name =?`,[post.searchAuthorName],function(error2,authors){
-        //db.query(`SELECT * FROM author where name Like '%t%'`,function(error2,authors){
-          console.log("*****************************************");
-          console.log(authors.length)
-          //db.query(`SELECT * FROM author where name LIKE ?`,['%'+post.searchAuthorName+'%'],function(error2,authors){
-          db.query(`SELECT * FROM author where name LIKE '%${post.searchAuthorName}%'`,function(error2,authors){
+          db.query(`SELECT * FROM author where name LIKE ?`,['%'+post.searchAuthorName+'%'],function(error2,authors){
             var title = 'author';
             var list = template.list(topics); // template.js에 있는 list프로퍼티의 함수
             var html = template.HTML(title, list, //template.html은 웹페이지의 가장 큰틀의 html코드를 만들어줌
