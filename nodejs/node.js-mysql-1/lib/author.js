0fad8f1cbd2b9b55d5e5911899ed0974c2f64826
kuyt1819 <kuyt1819@gmail.com>
2018-08-21 09:14:13 +0900

    nodejs 검색창 렙업중

--- nodejs/node.js-mysql-1/lib/author.js
+++ nodejs/node.js-mysql-1/lib/author.js
@@ -227,7 +227,8 @@ exports.search_author = function(request, response){
         console.log(post);
         console.log("-----------2222create_process log------------------");
     db.query(`SELECT * FROM topic`, function(error,topics){ //여기서 function은 쿼리문이 실행된후에 실행될 콜백 함수임
-        db.query(`SELECT * FROM author where name =?`,[post.searchAuthorName] ,function(error2,authors){
+        db.query(`SELECT * FROM author where name =?`,[post.searchAuthorName],function(error2,authors){
+          //db.query(`SELECT * FROM author where name LIKE=?`,['%'+post.searchAuthorName+'%'],function(error2,authors){
             var title = 'author';
             var list = template.list(topics); // template.js에 있는 list프로퍼티의 함수
             var html = template.HTML(title, list, //template.html은 웹페이지의 가장 큰틀의 html코드를 만들어줌
