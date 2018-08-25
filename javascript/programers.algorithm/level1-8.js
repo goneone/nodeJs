8a90a03d5bc50654346c83ccbbb7b75ed07b3f83
goneone <kuyt1819@gmail.com>
2018-08-25 23:58:41 +0900

    2

--- javascript/programers.algorithm/level1-8.js
+++ javascript/programers.algorithm/level1-8.js
@@ -3,9 +3,11 @@
 //내풀이
 
 function solution(s){
+    var answer;
     changeS = s.lowercase();
-
-    var answer = true;
+    if(changeS.indexOf("p")= -1 && changeS.indexOf("y")= -1 ) {
+       answer = true;
+    }
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     console.log('Hello Javascript')
