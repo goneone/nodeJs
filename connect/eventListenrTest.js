6a70a6e32bc92dc77db533eae6f65c2961abb134
goneone <kuyt1819@gmail.com>
2018-12-14 23:36:38 +0900

    commit

---
+++ connect/eventListenrTest.js
@@ -0,0 +1,7 @@
+//addEventListener는 이벤트를 등록하는거임. 클릭했을때의 이벤트를.
+//function 에 매개변수(evt)를 받을수 있음. 이벤트정보를 받는거임.
+var el = document.getElementById("outside");
+el.addEventListener("click", function(evt){
+ console.log(evt.target);
+ console.log(evt.target.nodeName);
+}, false);
